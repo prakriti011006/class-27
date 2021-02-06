2e0d7c4d25ca923164049f2ad70c0d246296acbb
prakriti011006 <neelam011006@gmail.com>
2021-02-06 17:50:25 +0530

    class 27

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var wood;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -19,6 +21,7 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -29,13 +32,14 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-
+    wood = new Log(450,102,100,PI/3)
     box5 = new Box(810,160,70,70);
+
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,wood.body)
 }
 
 function draw(){
@@ -60,5 +64,7 @@ function draw(){
     log5.display();
 
     bird.display();
+    wood.display();
+    chain.display();
     platform.display();
 }
